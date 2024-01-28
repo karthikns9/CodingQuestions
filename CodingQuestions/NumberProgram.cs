5ded82ee4806e764b57a0bb3f17685eb37c9e5e2
karthikns999 <karthikns999@gmail.com>
2024-01-28 22:57:18 +0530

    Math programs

--- CodingQuestions/NumberProgram.cs
+++ CodingQuestions/NumberProgram.cs
@@ -20,5 +20,105 @@ namespace CodingQuestions
                 num = quotient;
             }
         }
+
+        public static void CountDigits(int num)
+        {
+            int count = 0;
+            while(num > 0)
+            {
+                count++;
+                num = num / 10;
+            }
+
+            //string x = to_string(n);
+            //return x.length();
+
+            //int digits = Math.Floor(Math.Log10(n) + 1);
+            //return digits;
+        }
+
+        public static void ReverseNum(int num)
+        {
+            string reverseNum = string.Empty;
+            while(num > 0)
+            {
+                reverseNum = reverseNum + num % 10;
+                num = num / 10;
+
+                //int digit = N % 10;
+                //reverse = reverse * 10 + digit;
+                //N = N / 10;
+            }
+        }
+
+        public static void Palindrome(int num)
+        {
+            int reverseNum = 0;
+            int copy = num;
+            while(copy > 0)
+            {
+                reverseNum = reverseNum  * 10 + copy % 10;
+                copy = copy / 10;
+            }
+            if(num == reverseNum)
+            {
+                Console.Write("Palindrome");
+            }
+        }
+
+        public static void Amstrong(int num, int digit)
+        {
+            int copy = num;
+            var amstrongNum = 0D;
+
+            while (copy > 0)
+            {
+                var quotient = copy % 10;
+                var cube = Math.Pow(quotient, digit);
+                amstrongNum = amstrongNum + cube;
+                copy = copy / 10;
+            }
+            if (num == amstrongNum)
+            {
+                Console.Write("Amstrong Number");
+            }
+        }
+
+        public static void PrintAlDivisors(int num)
+        {
+            int copy = num;
+
+            for (int i = 1; i <= copy; i++)
+            {
+                if(copy % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static bool isPrime(int N)
+        {
+            for (int i = 2; i < N; i++)
+            {
+                if (N % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static void GCD(int num1, int num2)
+        {
+            int ans = 1;
+            for (int i = 1; i <= Math.Min(num1, num2); i++)
+            {
+                if (num1 % i == 0 && num2 % i == 0)
+                {
+                    ans = i;
+                }
+            }
+        }
     }
 }
--- CodingQuestions/Program.cs
+++ CodingQuestions/Program.cs
@@ -6,49 +6,48 @@ namespace CodingQuestions
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<StringBuilderReplaceTest>();
-
-            StringPrograms.ReverseString("Hello World");
-
-            StringPrograms.IsPalindrome("liril");
-
-            StringPrograms.ReverseTheOrderOfString("Welcome to Csharp corner");
-
-            StringPrograms.ReverseEachWordInString("Welcome to Csharp corner");
-
-            StringPrograms.CountEachOccurancesInString("hello world");
-
-            StringPrograms.CountNumberOfWords("Welcome to Csharp corner");
-
-            StringPrograms.RemoveDuplicateCharactersFromString("google");
-
-            StringPrograms.CheckAnagram("Silent", "Listen");
-
-            StringPrograms.FindAllPossibleSubStrings("abcd");
-
-            StringPrograms.HighestOccuredCharInString("hello");
-
-            StringPrograms.CheckIfStringHasUniqueCharacters("hello");
-
-            string s = StringPrograms.ReverseStringUsingRecursion("Hello");
-
-            //Array programs
-            int[] arr = { 1, 2, 3, 4, 5};
-            ArrayPrograms.FindDuplicates(arr);
-
-            //ArrayPrograms.RotateArrayLeft(arr);
-
-            ArrayPrograms.RotateArrayRight(arr);
-
-            int[] arr1 = { 3,2,1,5,4 };
-            ArrayPrograms.SecondLargest(arr1);
-
-            int[] arr2 = { 1,2,3,4 };
-            ArrayPrograms.TwoIntegersSumToTarget(arr2, 4);
-
-
             //Number programs
-            NumberProgram.SumOfNumbers(125);
+            //NumberProgram.SumOfNumbers(125);
+
+            NumberProgram.GCD(3, 6);
+
+            //StringPrograms.ReverseString("Hello World");
+
+            //StringPrograms.IsPalindrome("liril");
+
+            //StringPrograms.ReverseTheOrderOfString("Welcome to Csharp corner");
+
+            //StringPrograms.ReverseEachWordInString("Welcome to Csharp corner");
+
+            //StringPrograms.CountEachOccurancesInString("hello world");
+
+            //StringPrograms.CountNumberOfWords("Welcome to Csharp corner");
+
+            //StringPrograms.RemoveDuplicateCharactersFromString("google");
+
+            //StringPrograms.CheckAnagram("Silent", "Listen");
+
+            //StringPrograms.FindAllPossibleSubStrings("abcd");
+
+            //StringPrograms.HighestOccuredCharInString("hello");
+
+            //StringPrograms.CheckIfStringHasUniqueCharacters("hello");
+
+            //string s = StringPrograms.ReverseStringUsingRecursion("Hello");
+
+            ////Array programs
+            //int[] arr = { 1, 2, 3, 4, 5};
+            //ArrayPrograms.FindDuplicates(arr);
+
+            ////ArrayPrograms.RotateArrayLeft(arr);
+
+            //ArrayPrograms.RotateArrayRight(arr);
+
+            //int[] arr1 = { 3,2,1,5,4 };
+            //ArrayPrograms.SecondLargest(arr1);
+
+            //int[] arr2 = { 1,2,3,4 };
+            //ArrayPrograms.TwoIntegersSumToTarget(arr2, 4);
         }
     }
 }
