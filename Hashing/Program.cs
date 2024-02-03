b382a71f5b06a00ea1d9624abec741cf775e2160
karthikns999 <karthikns999@gmail.com>
2024-02-03 22:08:15 +0530

    Hashing

---
+++ Hashing/Program.cs
@@ -0,0 +1,55 @@
+﻿
+static void CountFrequencyOfElementInArray(int[] arr)
+{
+    Dictionary<int, int> keyValuePairs = new();
+
+    foreach (var pair in arr)
+    {
+        if (!keyValuePairs.TryAdd(pair, 1))
+        {
+            keyValuePairs[pair]++;
+        }
+    }
+    
+    foreach (var pair in keyValuePairs)
+    {
+        Console.WriteLine($"{pair.Key} ----> {pair.Value}");
+    }
+}
+
+static void FindHighAndLowFrequencyElement(int[] arr)
+{
+    Dictionary<int, int> keyValuePairs = new();
+
+    foreach (var pair in arr)
+    {
+        if (!keyValuePairs.TryAdd(pair, 1))
+        {
+            keyValuePairs[pair]++;
+        }
+    }
+    int max = Int32.MinValue;
+    int min = Int32.MaxValue;
+    int maxKey = Int32.MinValue;
+    int minKey = Int32.MaxValue;
+
+    foreach (var pair in keyValuePairs)
+    {
+        if (pair.Value > max)
+        {
+            max = pair.Value;
+            maxKey = pair.Key;
+        }
+
+        if (pair.Value < min)
+        {
+            min = pair.Value;
+            minKey = pair.Key;
+        }
+    }
+    Console.WriteLine($"Max Key is: {maxKey}, Min Key is: {minKey}");
+
+}
+int[] arr = { 2, 2, 3, 4, 4, 2 };
+
+FindHighAndLowFrequencyElement(arr);
